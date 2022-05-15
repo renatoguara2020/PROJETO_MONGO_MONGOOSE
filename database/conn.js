f908b8e63de4d81d96ff64ea88ad64a67eeb55de
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-15 11:45:59 -0300

    coomit changes

--- database/conn.js
+++ database/conn.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://localhost:27017/mongo_mongoose_testes";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
