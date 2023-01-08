2b4b4331083ef557396277db34830d5f1cfd2d3c
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-08 19:10:34 -0300

    ok

--- models/nodeMailer.js
+++ models/nodeMailer.js
@@ -3,16 +3,16 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
+    user: 'renatoguara2020@gmail.com',
+    pass: 'agciqpqrqferdazp'
   }
 });
 
 var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
+  from: 'renatoguara2020@gmail.com',
+  to: 'renatoguara2019@yahoo.com',
   subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  text: 'That was easy Renato Alves Soares!!!!!!!!!!!!'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
