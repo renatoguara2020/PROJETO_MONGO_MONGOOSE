1e30d1cb92ed5731e1db1e6d3beb51b9cbfeef95
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-09 09:33:43 -0300

    ok

--- models/nodeMailer.js
+++ models/nodeMailer.js
@@ -12,7 +12,7 @@ var mailOptions = {
   from: 'renatoguara2020@gmail.com',
   to: 'renatoguara2019@yahoo.com',
   subject: 'Sending Email using Node.js',
-  text: 'That was easy Renato Alves Soares!!!!!!!!!!!!'
+  text: 'That was easy Renato Alves Soares 08 54!!!!!!!!!!!!'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
